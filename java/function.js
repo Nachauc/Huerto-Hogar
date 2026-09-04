f00ca9fdc287a6e3b959554ce56438d78fb51949
Nachauc <ma.urrutia@duocuc.cl>
2026-09-04 15:10:44 -0400

    Creacion apartado nosotros, cambios en inicio de sesion y validaciones de inicio de sesion

---
+++ java/function.js
@@ -0,0 +1,311 @@
+console.log("hola");
+
+var productosEstrellas = [
+    {
+        "id": 1,
+        "nombre":"Manzana Fuji",
+        "imagen":"../img/manzana.png",
+        "precio":1200,
+        "stock":150,
+        "descripcion":"Manzanas Fuji crujientes y dulces, cultivadas en el Valle del Maule. Perfectas para meriendas saludables o como ingrediente en postres. Estas manzanas son conocidas por su textura firme y su sabor equilibrado entre dulce y ácido."
+
+    },
+    {
+        "id": 2,
+        "nombre":"Naranjas Valencia",
+        "imagen":"../img/naranja.png",
+        "precio":1000,
+        "stock":200,
+        "descripcion":"Jugosas y ricas en vitamina C, estas naranjas Valencia son ideales para zumos frescos y refrescantes. Cultivadas en condiciones climáticas óptimas que aseguran su dulzura y jugosidad."
+
+    },
+    {
+        "id": 3,
+        "nombre":"Plátanos Cavendish",
+        "imagen":"../img/platano.png",
+        "precio":800,
+        "stock":250,
+        "descripcion":"Plátanos maduros y dulces, perfectos para el desayuno o como snack energético. Estos plátanos son ricos en potasio y vitaminas, ideales para mantener una dieta equilibrada."
+
+    },
+    {
+        "id": 4,
+        "nombre":"Zanahorias Orgánicas",
+        "imagen":"../img/zanahoria.png",
+        "precio":900,
+        "stock":100,
+        "descripcion":"Zanahorias crujientes cultivadas sin pesticidas en la Región de O'Higgins. Excelente fuente de vitamina A y fibra, ideales para ensaladas, jugos o como snack saludable."
+
+    },
+    {
+        "id": 5,
+        "nombre":"Espinacas Frescas",
+        "imagen":"../img/espinaca.png",
+        "precio":700,
+        "stock":80,
+        "descripcion":"Espinacas frescas y nutritivas, perfectas para ensaladas y batidos verdes. Estas espinacas son cultivadas bajo prácticas orgánicas que garantizan su calidad y valor nutricional."
+
+    },
+    {
+        "id": 6,
+        "nombre":"Pimientos Tricolores",
+        "imagen":"../img/pimenton.png",
+        "precio":1500,
+        "stock":120,
+        "descripcion":"Pimientos rojos, amarillos y verdes, ideales para salteados y platos coloridos. Ricos en antioxidantes y vitaminas, estos pimientos añaden un toque vibrante y saludable a cualquier receta."
+
+    }
+]
+
+    //NUEVO 🙀
+
+    function actuaizarContador(){
+        const contadorElemento = document.getElementById("contadorCarrito");
+        if (contadorElemento) {
+            let carritoActual = JSON.parse(localStorage.getItem("carrito")) || [];
+            contadorElemento.textContent = carritoActual.length > 0 ? `(${carritoActual.length})` : "";
+        }
+    }
+    function agregarAlCarrito(producto){
+        let carritoActual = JSON.parse(localStorage.getItem("carrito")) || [];
+        carritoActual.push(producto);
+        localStorage.setItem("carrito", JSON.stringify(carritoActual))
+        actuaizarContador();
+        alert(producto.nombre + "se ha añadido al carrito.");
+    }
+    
+const section = document.getElementById("productosEstrella");
+if (section) {
+    const contenedorCards = document.createElement("div");
+    contenedorCards.className = "contenedor-cards row g-3 justify-content-center";
+    section.appendChild(contenedorCards);
+
+    for (const i of productosEstrellas) {
+        const col = document.createElement("div");
+        col.className = "col-12 col-sm-6 col-md-4 col-lg-2";
+
+        const card = document.createElement("div");
+        card.className = "card h-100 d-flex flex-column text-center p-2";
+        
+        const nombreProducto = document.createElement("h5");
+        nombreProducto.textContent = i.nombre;
+        nombreProducto.className = "titulo-producto fs-6 fw-bold";
+        card.appendChild(nombreProducto);
+
+        const imagenProducto = document.createElement("img");
+        imagenProducto.src = i.imagen;
+        imagenProducto.className = "img-fluid mb-2";
+        imagenProducto.style.maxHeight = "100px";
+        imagenProducto.style.objectFit = "contain";
+        card.appendChild(imagenProducto);
+
+        const precioProducto = document.createElement("p");
+        precioProducto.className = "precio-producto text-success fw-bold mt-1 mb-2";
+        precioProducto.textContent = "$ " + i.precio;
+        card.appendChild(precioProducto);
+
+        const contenedorBoton = document.createElement("div"); 
+        contenedorBoton.className = "contenedor-boton mt-auto";
+
+        
+        const botonAgregar = document.createElement("button");
+        botonAgregar.textContent = "Añadir";
+        botonAgregar.className = "btn btn-success btn-sm w-100";
+        botonAgregar.addEventListener("click", function(){
+            agregarAlCarrito(i);
+        });
+
+        contenedorBoton.appendChild(botonAgregar);
+    
+    
+        card.appendChild(contenedorBoton)
+        col.appendChild(card);
+        contenedorCards.appendChild(col);
+    }
+}
+
+actuaizarContador();
+
+const contenedorCarrito = document.getElementById("contenedorCarrito");
+const totalPagarElemento = document.getElementById("totalPagar")
+
+if (contenedorCarrito) {
+    let carritoGuardado = JSON.parse(localStorage.getItem("carrito")) || [];
+    let total = 0;
+    contenedorCarrito.innerHTML = "";
+
+    if (carritoGuardado.length === 0){
+        const mensajeVacio = document.createElement("p");
+        mensajeVacio.textContent = "Tu carrito esta vacio";
+        mensajeVacio.className = "text-center fs-4 mt-4 text-muted";
+        contenedorCarrito.appendChild(mensajeVacio);
+    } else {
+        for (const item of carritoGuardado){
+            total += item.precio;
+
+            const card = document.createElement("div");
+            card.className = "card mb-3 w-100 shadow-sm";
+            card.style.maxWidth = "500px";
+
+            const row = document.createElement("div");
+            row.className = "row g-0 align-items-center p-2";
+            card.appendChild(row);
+
+            const colImg = document.createElement("div");
+            colImg.className = "col-4 text-center";
+            const img = document.createElement("img");
+            img.src = item.imagen;
+            img.className = "img-fluid rounded";
+            img.style.maxHeight = "80px";
+            colImg.appendChild(img);
+            row.appendChild(colImg);
+
+            const colText = document.createElement("div");
+            colText.className = "col-8";
+
+            const cardBody = document.createElement("div");
+            cardBody.className = "card-body py-1";
+
+            const titulo = document.createElement("h5");
+            titulo.className = "card-tittle mb-1";
+            titulo.textContent = item.nombre;
+
+            const precio = document.createElement("p");
+            precio.className = "card-text fw-bold text-success mb-0";
+            precio.textContent = "$ "+item.precio;
+
+            cardBody.appendChild(titulo);
+            cardBody.appendChild(precio);
+            colText.appendChild(cardBody);
+            row.appendChild(colText);
+
+            contenedorCarrito.appendChild(card);
+        }
+    }
+    if (totalPagarElemento) {
+        totalPagarElemento.textContent = "Total: $" + total;
+    }
+}
+//NUEVO 🙀
+
+
+
+//VALIDACIONES CREAR USUARIO//
+
+document.addEventListener("DOMContentLoaded",() =>{
+
+ const btnEnviar = document.getElementById("btnEnviar");
+
+ btnEnviar.addEventListener("click",()=>{
+
+    let valido = true;
+
+    /*validacion run*/
+    const run= document.getElementById("inputRun").value.trim().toUpperCase();
+    const errorRun = document.getElementById("errorRun");
+
+    if(run ==""){
+        errorRun.textContent= "El run es obligatorio.";
+        valido= false;
+    
+    }else if (run.length <7 || run.length >9){
+        errorRun.textContent = "El run debe tener entre 7 y 9 caracteres."
+        valido= false;
+
+    }else if (!/^[0-9]+[0-9K]$/.test(run)){
+        errorRun.textContent = "El run debe tener numeros y un digito"
+        valido=false;
+
+
+    }else{
+        errorRun.textContent="El run no es Valido"
+    }
+
+    /*validacion nombre y apellido*/
+
+    const nombre = document.getElementById("inputNombre").value.trim();
+    const errorNombre = document.getElementById("errorNombre");
+
+    if (nombre==""){
+        errorNombre.textContent="El nombre es obligatorio";
+        valido = false;
+
+    }else if (nombre.length>50){
+        errorNombre.textContent ="Maximo 50 caracteres.",
+        valido = false;
+    }else{
+        errorNombre.textContent = "";
+    }
+
+    const apellido = document.getElementById("inputApellidos").value.trim();
+    const errorApellido = document.getElementById("errorApellido");
+
+    if(apellidos == ""){
+        errorApellido.textContent = "Los apellidos son obligatorio";
+        valido = false;
+
+    }else if (apellido.length >100){
+        errorApellido.textContent = "Maximo 100 caracteres."
+        valido = false;
+    }else{
+        errorApellido.textContent = "";
+    }
+    
+    /*validacion Correo*/
+
+    const correo = document.getElementById("inputCorreo").value.trim();
+    const errorCorreo = document.getElementById("ErrorCorreo");
+    const dominiosPermitidos = ["duoc.cl","profesor.duoc.cl","gmail.com"];
+    const dominoCorreo = correo.split("@")[1];
+
+    if(correo == ""){
+        errorCorreo.textContent = "El correo es Obligatorio.";
+        valido = false;
+
+    }else if(correo.length >100){
+        errorCorreo.textContent = "Maximo 100 caracteres";
+        valido = false;
+
+
+    }else if(!dominiosPermitidos.includes(dominoCorreo)){
+        errorCorreo.textContent ="Solo se permiten correo @duoc.cl,@profesor.duoc.cl,@gmail.com";
+        valido = false;
+    }else{
+        errorCorreo.textContent = " El correo no es valido"
+    }
+
+
+    const seleccionRegion = document.getElementById("SeleccionRegion");
+    const seleccionComuna = document.getElementById("seleccionComuna");
+
+
+    RegionesComunas.forEach((item,index) =>{
+        const opcion = document.createElement("option");
+        opcion.value = index;
+        opcion.textContent = item.region;
+        seleccionRegion.appendChild(opcion);
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if(valido){
+        window.location.href = "main-logout.html";
+    }
+
+
+
+ });
+});
+
+
---
+++ java/regiones.js
@@ -0,0 +1,12 @@
+const RegionesComunas = [
+    {
+        region: "Region Metropolitana de Santiago",
+        comunas: ["Santiago","Providencia","Maipu","Pudahuel","Puente Alto","Las Condes","Estacion Central","Colina","Vitacura","Huechuraba","Quinta Normal","La Reina"]
+
+
+    },
+    {
+        region:"Region de Ñuble",
+        comunas:["Chillán","Bulnes","Chillán Viejo","Pemuco","San Carlos"]
+    }
+]
